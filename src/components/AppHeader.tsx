ed895bfb1b3c8015966eed10c68301058c14ed23
BossBSJ <bossllsj555@gmail.com>
2022-12-25 13:35:11 +0700

    add component

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,27 +1,37 @@
-import { Box, Button, Container } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { Box, Button, Container, Typography } from '@mui/material';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import logo from '../assets/logo.svg'
 
 const AppHeader = () => {
+    const navigate = useNavigate()
     return (
         <Box sx={{backgroundColor:"rgba(255,255,255,0.40)"}}>
             <Container sx={{padding: "10px", display:"flex", justifyContent: "space-between"}}>
-                <Box sx={{padding: "20px"}}>
-                    <NavLink to="/" style={{textDecoration:"none" ,color:"black"}}>
+                <Box 
+                    sx={{padding: "20px", cursor:"pointer"}} 
+                    onClick={() => {navigate(`/`)}}
+                    >
                         <img 
                             src={logo}
                             style={{width:"120px"}}
                         />
-                    </NavLink>
                 </Box>
-                <Box sx={{display:"flex"}}>
-                    <NavLink to="/createIndexes" style={{textDecoration:"none"}}>
-                        <Button sx={{padding: "20px", borderRadius:"20px"}} variant="outlined">
-                            Create Indexes
-                        </Button>
-                    </NavLink>
-                    <Box sx={{padding: "20px"}}>
+                <Box sx={{display:"flex", justifyContent:"space-around", width:"500px"}}>
+                    <Typography
+                        sx={{padding:"40px", cursor:"pointer", color:"#005FFF"}}
+                        onClick={() => {navigate(`myPort`)}}
+                    >
+                        My port
+                    </Typography>
+                    <Button
+                        variant="outlined" 
+                        sx={{margin:"auto"}}
+                        onClick={() => {navigate(`createIndex`)}}
+                    >
+                        <Typography sx={{fontWeight:"bold", color:"#627DFF"}}>Create Indexes</Typography>
+                    </Button>
+                    <Box sx={{margin:"auto"}}>
                         <ConnectButton/>
                     </Box>
                 </Box>
