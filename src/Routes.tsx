10bcf772f07a18e262077863c4888a800b657363
BossBSJ <bossllsj555@gmail.com>
2022-12-25 13:15:59 +0700

    add path myport and indexDetail

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom"
 import IndexDetail from "./components/indexDetail"
-import CreateIndexes from "./pages/createIndexes"
+import CreateIndex from "./pages/createIndex"
+import MyPort from "./pages/myPort"
+import NotFound from "./pages/notFound"
 import SelectIndexes from "./pages/selectIndexes"
 
 
 const AppRoutes = () => {
     return (
         <Routes>
+            <Route path="*" element={<NotFound/>}/>
             <Route path="/" element={<SelectIndexes/>}/>
-            <Route path="/createIndexes" element={<CreateIndexes/>}/>
-            <Route path="/indexDetail" element={<IndexDetail/>}/>
+            <Route path="/createIndex" element={<CreateIndex/>}/>
+            <Route path="/myPort" element={<MyPort/>}/>
+            <Route path="/indexDetail/:indexId" element={<IndexDetail/>}/>
         </Routes>
     )
 }
