72ff0892142e423466fcf60e762e14c2d91c7767
BossBSJ <bossllsj555@gmail.com>
2022-12-29 01:25:15 +0700

    re directory and flow
    Co-authored-by: PoomSC <sakchai.wachi@gmail.com>

--- src/components/views/createIndex/CreateIndex.tsx
+++ src/components/views/createIndex/CreateIndex.tsx
@@ -2,7 +2,9 @@ import { Button, Card, Container } from "@mui/material";
 import { Box } from "@mui/system"
 import React, { useState } from "react";
 import CreateStep from "./CreateStep";
-import SetupIndex from "./setupIndex/SetupIndex";
+import ChooseTokenCard from "./setupIndex/ChooseTokenCard";
+import DeployCard from "./setupIndex/DeployCard";
+import SetupIndexCard from "./setupIndex/SetupIndexCard";
 
 
 
@@ -13,14 +15,37 @@ const CreateIndex = () => {
         setCreateIndexCard(true)
     }
 
+    const [page, setPage] = useState<number>(0)
+    const handleOnNext = () => {
+        if(page == 3) 
+            return
+        setPage(page + 1)
+    }
+
+    const handleOnBack = () => {
+        if(page == 0) 
+            return
+        setPage(page - 1)
+    }
+
     return (
         <Box>
             <Container sx={{display:"flex"}}>
                 <Box>
                     <CreateStep/>
-                    {!createIndexCard? <Button variant="contained" onClick={handleOpenCreateIndexCard}>Get Started</Button> : null}
+                    {page==0 && <Button variant="contained" onClick={handleOnNext}>Get Started</Button>}
                 </Box>
-                {createIndexCard? <SetupIndex/> : null}
+                {page!=0 &&
+                    <Card sx={{backgroundColor:"rgba(255,253,251,0.48)", width:"600px", height:"700px", display:"flex", flexDirection:"column", justifyContent:"space-between", paddingBottom:"20px"}}>
+                        {page==1 && <ChooseTokenCard/>}
+                        {page==2 && <SetupIndexCard/>}
+                        {page==3 && <DeployCard/>}
+                        <Box sx={{display:"flex", justifyContent:"space-around"}}>
+                            <Button onClick={handleOnBack} variant="outlined" sx={{width:"240px"}}>Back</Button>
+                            <Button onClick={handleOnNext} variant="contained" sx={{width:"240px"}}>{page==3? "Deploy Contract": "Next"}</Button>
+                        </Box>
+                    </Card>
+                }
             </Container>
         </Box>
     )
