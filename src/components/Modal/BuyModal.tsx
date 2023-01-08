bf6f673e4e4f2e2782cf39e3de2f2ac2d8097ba4
BossBSJ <bossllsj555@gmail.com>
2023-01-08 23:14:21 +0700

    add DCA part

--- src/components/Modal/BuyModal.tsx
+++ src/components/Modal/BuyModal.tsx
@@ -3,13 +3,13 @@ import { Button, ButtonGroup, Card, CardHeader, InputBase, MenuItem, Modal, Pape
 import { Box } from "@mui/system"
 import { ChangeEvent, useEffect, useState } from "react";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import theme from "../../theme";
 
 type IProps = {
     open: boolean
     onClose: () => void
     dcaModal: boolean
 }
-
 
 const BuyModal = (props: IProps) => {
     const { open, onClose, dcaModal } = props
@@ -25,53 +25,63 @@ const BuyModal = (props: IProps) => {
     }
 
     const [periodDCA, setPeriodDCA] = useState<string>('')
-    const handleChoosePeriod = (plan:string) => (event: any) => {
-        // console.log(plan)
-        setPeriodDCA(plan)
+    const handleChoosePeriod = (plan:string) => {
+        if(periodDCA == plan){
+            setPeriodDCA('')
+        }else {
+            setPeriodDCA(plan)
+        }
+        
     }
-
-    const primaryGradient = "linear-gradient(99.29deg, #004FD0 -23.14%, #3280FF 32.47%, #5E7DFF 79.6%, #7A7AFF 122.55%)"
 
     return(
         <Modal
             open={open}
             onClose={onClose}
         >
-            <Card sx={{backgroundColor:"#F3F3FF",width:"25vw", height:"40vh", padding: "20px", borderRadius:"16px", position:"absolute", left:"50%", top:"50%", transform: "translate(-50%, -50%)"}}>
-                <Typography variant="h5" sx={{fontWeight:"bold", textAlign:"center"}}>Buy</Typography>
-                <Typography variant="caption">Receive</Typography>
-                <Box sx={{display:"flex" ,justifyContent:"space-between"}}>
-                    <Typography>DPI</Typography>
-                    <TextField
-                        type="number"
-                        placeholder="0.0"
-                        inputProps={{
-                            sx: {textAlign: "right","&::placeholder": {textAlign: "right",}},
-                        }}
-                    />
-                </Box>
-                <Typography variant="caption">Pay</Typography>
-                <Box sx={{display:"flex" ,justifyContent:"space-between"}}>
-                    <Select value={currency} onChange={handleCurrencyChange} IconComponent={ExpandMoreIcon} variant="standard"
-                        sx={{float:"right", background:"white", borderRadius:"8px", backgroundColor:"#F3F3FF"}}
-                    >
-                        <MenuItem value={"USD"}>
-                            <Typography>USD</Typography>
-                        </MenuItem>
-                        <MenuItem value={"ETH"}>
-                            <Typography>BTC</Typography>
-                        </MenuItem>
-                        <MenuItem value={"BTC"}>
-                            <Typography>ETH</Typography>
-                        </MenuItem>
-                    </Select>
-                    <TextField
-                        type="number"
-                        placeholder="0.0"
-                        inputProps={{
-                            sx: {textAlign: "right","&::placeholder": {textAlign: "right",}},
-                        }}
-                    />
+            <Card sx={{backgroundColor:"#F3F3FF",width:"25vw", padding: "20px", position:"absolute", left:"50%", top:"50%", transform: "translate(-50%, -50%)"}}>
+                <Typography variant="h5" sx={{fontWeight:"bold", textAlign:"center"}}>
+                    {/* {dcaModal? "But with DCA":"Buy"} */}
+                    Buy
+                </Typography>
+                <Box sx={{display:"flex", flexDirection:"column", justifyContent:"space-between"}}>
+                    <Box>
+                        <Typography variant="caption">Receive</Typography>
+                        <Box sx={{display:"flex" ,justifyContent:"space-between"}}>
+                            <Typography>DPI</Typography>
+                            <TextField
+                                type="number"
+                                placeholder="0.0"
+                                inputProps={{
+                                    sx: {textAlign: "right","&::placeholder": {textAlign: "right",}},
+                                }}
+                            />
+                        </Box>
+                    </Box>
+                <Box>
+                    <Typography variant="caption">Pay</Typography>
+                    <Box sx={{display:"flex" ,justifyContent:"space-between"}}>
+                        <Select value={currency} onChange={handleCurrencyChange} IconComponent={ExpandMoreIcon} variant="standard"
+                            sx={{float:"right", background:"white", borderRadius:"8px", backgroundColor:"#F3F3FF"}}
+                        >
+                            <MenuItem value={"USD"}>
+                                <Typography>USD</Typography>
+                            </MenuItem>
+                            <MenuItem value={"ETH"}>
+                                <Typography>BTC</Typography>
+                            </MenuItem>
+                            <MenuItem value={"BTC"}>
+                                <Typography>ETH</Typography>
+                            </MenuItem>
+                        </Select>
+                        <TextField
+                            type="number"
+                            placeholder="0.0"
+                            inputProps={{
+                                sx: {textAlign: "right","&::placeholder": {textAlign: "right",}},
+                            }}
+                        />
+                    </Box>
                 </Box>
                 <Box sx={{display:"flex" ,justifyContent:"space-between"}}>
                     <Typography sx={{fontWeight:"bold"}}>DCA With DPI</Typography>
@@ -80,36 +90,53 @@ const BuyModal = (props: IProps) => {
                         onChange={handleChangeSwitch}
                     />
                 </Box>
-                {checked?
+                {checked &&
                 <Box sx={{display:"flex", justifyContent:"space-around"}}>
-                    <ButtonGroup sx={{borderRadius:"8px"}}>
+                    <ButtonGroup sx={{borderRadius:"8px",}}>
                         <Button 
                             variant="text" 
-                            sx={periodDCA == 'Day'?{background:"linear-gradient(99.29deg, #004FD0 -23.14%, #3280FF 32.47%, #5E7DFF 79.6%, #7A7AFF 122.55%)", color:"white"}:{}}
-                            onClick={handleChoosePeriod('Day')}
+                            sx={periodDCA == 'Day'?{background:theme.palette.gradient.primary, color:"white"}:{}}
+                            onClick={() => handleChoosePeriod('Day')}
                         >
                             Day
                         </Button>
                         <Button 
                             variant="text" 
-                            sx={periodDCA == 'Weekly'?{background:"linear-gradient(99.29deg, #004FD0 -23.14%, #3280FF 32.47%, #5E7DFF 79.6%, #7A7AFF 122.55%)", color:"white"}:{}}
-                            onClick={handleChoosePeriod('Weekly')}
+                            sx={periodDCA == 'Weekly'?{background:theme.palette.gradient.primary, color:"white"}:{}}
+                            onClick={() => handleChoosePeriod('Weekly')}
                         >                            
                             Weekly
                         </Button>
                         <Button 
                             variant="text" 
-                            sx={periodDCA == 'Monthly'?{background:"linear-gradient(99.29deg, #004FD0 -23.14%, #3280FF 32.47%, #5E7DFF 79.6%, #7A7AFF 122.55%)", color:"white"}:{}}
-                            onClick={handleChoosePeriod('Monthly')}
+                            sx={periodDCA == 'Monthly'?{background:theme.palette.gradient.primary, color:"white"}:{}}
+                            onClick={() => handleChoosePeriod('Monthly')}
                         >                            
                             Monthly
                         </Button>
                     </ButtonGroup>
-                </Box> : ""}
+                </Box>
+                }
+                {periodDCA != '' && checked &&
+                <Box>
+                    <Typography sx={{fontWeight:"bold"}}>Deposit to our vault</Typography>
+                    <Box sx={{display:"flex", justifyContent:"space-between"}}>
+                        <TextField
+                                type="number"
+                                placeholder="0.0"
+                                inputProps={{
+                                    sx: {textAlign: "right","&::placeholder": {textAlign: "right",}},
+                                }}
+                        />
+                        <Typography>est. 3.2 {periodDCA}</Typography>
+                    </Box>
+                </Box>}
                 <Box sx={{display:"flex", justifyContent:"space-around"}}>
                     <Button onClick={onClose} variant="contained" sx={{width:"320px"}}>
                         <Typography sx={{fontWeight:"bold"}}>BUY DPI</Typography>
+                        
                     </Button>
+                </Box>
                 </Box>
             </Card>
         </Modal>
@@ -117,4 +144,3 @@ const BuyModal = (props: IProps) => {
 }
 
 export default BuyModal
-
