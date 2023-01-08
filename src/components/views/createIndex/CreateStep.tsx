327dddbb9793de8795f9426dd72db84ab5d5a160
BossBSJ <bossllsj555@gmail.com>
2023-01-08 23:14:32 +0700

    add style

--- src/components/views/createIndex/CreateStep.tsx
+++ src/components/views/createIndex/CreateStep.tsx
@@ -1,29 +1,63 @@
 import { Button, Container, Typography } from "@mui/material"
 import { Box } from "@mui/system"
+import theme from "../../../theme"
 
-const CreateStep = () => {
+type IProps = {
+    page: number
+}
+
+const CreateStep = (props: IProps) => {
+    const {page} = props
+    // console.log(page)
+    
+    const activeColorStyle = {
+        background:theme.palette.gradient.secondary, 
+        width:"46px", 
+        height:"46px", 
+        borderRadius:"50%",
+        display: 'flex', 
+        alignItems: 'center',
+        justifyContent:"center",
+    }
+
+    const inActiveColorStyle = {
+        background:theme.palette.secondary.main, 
+        width:"46px", 
+        height:"46px", 
+        borderRadius:"50%",
+        display: 'flex', 
+        alignItems: 'center',
+        justifyContent:"center"
+    }
+
     return(
-        <Box sx={{margin:"auto", display:"flex", flexDirection:"column"}}>
-            <Typography variant="h3" sx={{fontWeight:"bold"}}>Create Your Index</Typography>
+        <Box sx={{margin:"auto", display:"flex", flexDirection:"column", height:"400px", justifyContent:"space-evenly"}}>
+            <Typography variant="h3" sx={{fontWeight:"bold", color:"#1D144F"}}>Create Your Index</Typography>
             <Box sx={{display:"flex"}}>
-                <Typography variant="h5" sx={{fontWeight:"bold"}}>1</Typography>
-                <Box>
-                    <Typography variant="body1" sx={{fontWeight:"bold"}}>Choose Tokens List</Typography>
-                    <Typography variant="body2">Select tokens to include in your Index. These can be changed later.</Typography>
+                <Box sx={page==1 || page==0? activeColorStyle: inActiveColorStyle}>
+                    <Typography variant="h5" sx={{fontWeight:"bold", color:"white"}}>1</Typography>
+                </Box>
+                <Box sx={{marginLeft:"15px"}}>
+                    <Typography variant="body1" sx={{fontWeight:"bold", color:"#1D144F"}}>Choose Tokens List</Typography>
+                    <Typography variant="body2" sx={{color:"#9B97B3"}}>Select tokens to include in your Index. These can be changed later.</Typography>
                 </Box>
             </Box>
             <Box sx={{display:"flex"}}>
-                <Typography variant="h5" sx={{fontWeight:"bold"}}>2</Typography>
-                <Box>
-                    <Typography variant="body1" sx={{fontWeight:"bold"}}>Setup Your Index</Typography>
-                    <Typography variant="body2">Add a name, symbol and starting price for your Set.</Typography>
+                <Box sx={page==2 || page==0? activeColorStyle: inActiveColorStyle}>
+                    <Typography variant="h5" sx={{fontWeight:"bold", color:"white"}}>2</Typography>
+                </Box>
+                <Box sx={{marginLeft:"15px"}}>
+                    <Typography variant="body1" sx={{fontWeight:"bold", color:"#1D144F"}}>Setup Your Index</Typography>
+                    <Typography variant="body2" sx={{color:"#9B97B3"}}>Add a name, symbol and starting price for your Set.</Typography>
                 </Box>
             </Box>
             <Box sx={{display:"flex"}}>
-                <Typography variant="h5" sx={{fontWeight:"bold"}}>3</Typography>
-                <Box>
-                    <Typography variant="body1" sx={{fontWeight:"bold"}}>Deploy smart contracts</Typography>
-                    <Typography variant="body2">Your smart contracts will be published to the Avalanche mainnet.</Typography>
+                <Box sx={page==3 || page==0? activeColorStyle: inActiveColorStyle}>
+                    <Typography variant="h5" sx={{fontWeight:"bold", color:"white"}}>3</Typography>
+                </Box>
+                <Box sx={{marginLeft:"15px"}}>
+                    <Typography variant="body1" sx={{fontWeight:"bold", color:"#1D144F"}}>Deploy smart contracts</Typography>
+                    <Typography variant="body2" sx={{color:"#9B97B3"}}>Your smart contracts will be published to the Avalanche mainnet.</Typography>
                 </Box>
             </Box>
         </Box>
